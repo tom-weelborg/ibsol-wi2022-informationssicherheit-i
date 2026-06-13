a4209c8d3e488810c17e3add6fcd737f23b7a1b2
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-06-14 00:14:43 +0200

    feat: add footer

---
+++ global-bottom.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="absolute bottom-0 left-5 right-5 h-16 flex">
+        <div class="w-1/4 text-left"></div>
+        <div class="w-2/4 text-center">Tom Weelborg</div>
+        <div class="w-1/4 text-right">{{ $nav.currentPage }}</div>
+    </footer>
+</template>
